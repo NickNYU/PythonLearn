bf3760bd18a68eb5f58c6f92d6698a74b4cf10f4
Nick Zhu <Nick.Zhu@emc.com>
2016-12-29 18:06:51 +0800

    Implement decorators

---
+++ decorator/try_catch_wrapper.py
@@ -0,0 +1,33 @@
+
+
+
+class A:
+    # Decorator as a class method
+    @classmethod
+    def decorator2(cls, func):
+        def wrapper(*args, **kwargs):
+            print 'Decorator 2'
+            try:
+                print 'now in try'
+                response = func(*args, **kwargs)
+                if response.ok:
+                    return response
+                else:
+                    print 'vRO call fails'
+            except Exception, e:
+                print 'now in exception'
+                print e
+        return wrapper
+
+
+# As a class method
+@A.decorator2
+def grok(url):
+    response = open('test', 'r')
+    return response
+
+if __name__ == '__main__':
+    url = 'https://192.168.3.119:8281/vco/api/inventory/System/Resources/Library/DynamicTypes/Configuration/'
+    #auth = auth.HTTPBasicAuth('app_vco_vcenter@vlab.local', 'Password123!')
+    response = grok(url)
+
